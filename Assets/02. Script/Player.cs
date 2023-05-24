5fb7d4c11f1ecd4e10e60dae187e8d77fee587fb
thkstyle <124120885+Bluepeacock4@users.noreply.github.com>
2023-05-24 23:43:13 +0900

    추가 : 플레이어 이동 구현
    플레이어 횡 이동, 점프 구현
    플레이어 피격, 공격 구현
    
    23.05.24
    
    김태현

---
+++ Assets/02. Script/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int life = 3;
+    public float speed;
+    public float jumpPower;
+    public float staggerValue;
+}
+
