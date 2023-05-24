efe9d1e22a4ccd686621b9c54ae4c0e89bf9602f
redflag-HSH <66863292+redflag-HSH@users.noreply.github.com>
2023-05-24 23:35:00 +0900

    AI WIP
    AI WIP

---
+++ Assets/02. Script/01.AIInterAct/AIBase.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBase : MonoBehaviour
+{
+    [SerializeField] float walkSpeed, chaseSpeed;
+    int AIMode, stare;
+    bool AIActive;
+    [SerializeField] float awareTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AIActive)
+        {
+            switch (AIMode)
+            {
+                case 0:
+                    //발 밑으로 레이캐스트 쏴서 확인
+                    //레이캐스트에 닿으면 대기걸고 3으로 변환
+                    RaycastHit2D EndCheck = Physics2D.Raycast(transform.position, new Vector2(0, -1), 2.0f/*, 레이어 */);
+                    if (EndCheck.collider != null)
+                        AIMode = 3;
+                    //아니면 이동
+                    else
+                        transform.Translate(new Vector2(stare * walkSpeed * Time.deltaTime, 0));
+                    break;
+                case 1:
+                    //플레이어 위치가 적이 향하는 방향이랑 일치하는지 확인
+                    //아니면 3으로
+
+                    //그냥 이동
+                    transform.Translate(new Vector2(stare * chaseSpeed * Time.deltaTime, 0));
+                    break;
+                case 2:
+                    //애니메이션 틀기
+
+                    //대기걸기
+                    //StartCoroutine(AIResetWait());
+                    break;
+                case 3:
+                    stare *= -1;
+                    //stare값이랑 스프라이트 방향 일치하는지 확인
+
+                    break;
+            }
+
+        }
+
+        if (awareTime > 0)
+        {
+            awareTime -= Time.deltaTime;
+        }
+
+        //정면에 박스 레이캐스트 발사
+
+        //
+    }
+
+    IEnumerator AIResetWait(float sec)
+    {
+        AIActive = false;
+        yield return new WaitForSeconds(sec);
+        AIMode = 0;
+        AIActive = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //플레이어 공격인지 확인
+        //if ()
+            //OnHit();
+    }
+
+    public void OnHit()
+    {
+        //플레이어 위치 확인
+
+        //앞에서 맞음
+        //스턴 애니메이션
+
+        //대기
+        //AIResetWait();
+        //뒤에서 맞음
+        //사망 애니메이션
+
+
+    }
+
+    public void OnAware()
+    {
+
+    }
+
+    public void OnAwareBackSide()
+    {
+
+    }
+}
+/*
+         AI베이스
+        프레임당 호출
+        대기상황용 bool 변수의 if문으로 감싸져있음 
+
+        0은 배회상태
+        문제없을 경우 이동
+        난간 끝일때 대기 상태 걸고 상태 3으로 
+
+        1은 플레이어 인지상태
+        대기상태는 코루틴
+
+        2는 공격상태
+        공격 애니메이션 재생하고 대기걸고 상태 1로
+
+        3은 고개 돌리기
+        방향 뒤집고 바로 0으로
+
+
+        기타 상호작용
+        플레이어가 인지 범위 밖으로 벗어나면 float변수로 카운트
+
+
+        피격 시
+        플레어이의 앞뒤 위치 확인
+            앞이면
+            스턴상태의 애니메이션으로 변경하고 대기걸기
+            뒤면
+            HP--하고 죽을지 확인
+        죽을 시
+        자신이 속한 플랫폼에서 자신을 제거
+        제거된것을 매니저에 전달
+
+
+
+         */
+
+//플랫폼의 스크립트에서 스폰된 적 리스트 할당
