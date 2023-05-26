abca1617b15e9417b2db9720ad9b2ca51e5ba240
thkstyle <124120885+Bluepeacock4@users.noreply.github.com>
2023-05-27 03:34:41 +0900

    추가 : 플레이어 기능 추가
    플레이어 순간이동 구현
    피격 시 무적 구현
    게임 설정 구현 ( 오디오믹서 추가 X )
    
    23.05.27
    
    김태현

---
+++ Assets/02. Script/SingleTon/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+
+    public bool isPause;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static GameManager Instance => _instance == null ? null : _instance;
+
+    public void PauseGame()
+    {
+        if (!isPause)
+        {
+            Time.timeScale = 0;
+            isPause = true;
+        }
+        else
+        {
+            ContinueGame();
+        }
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        isPause = false;
+    }
+
+    public void EnterTitle()
+    {
+        ContinueGame();
+        AudioManager.Instance.Click();
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void OpenOption()
+    {
+        AudioManager.Instance.OpenPannel();
+    }
+}
